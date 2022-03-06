b9462e01b9ab4739c583bdc75785603c37bceeaf
Кузнецов Алексей Эдуардович <akuznetsov@mpl.lc>
2022-03-06 09:39:47 +0300

    #4 Create domain layer of application

--- app/src/main/java/com/github/kadehar/currencyrate/feature/main_screen/data/Mapper.kt
+++ app/src/main/java/com/github/kadehar/currencyrate/feature/main_screen/data/Mapper.kt
@@ -1,6 +1,7 @@
 package com.github.kadehar.currencyrate.feature.main_screen.data
 
 import com.github.kadehar.currencyrate.feature.main_screen.data.api.model.CurrencyRateRemote
+import com.github.kadehar.currencyrate.feature.main_screen.domain.model.CurrencyRate
 
 fun CurrencyRateRemote.toDomain(): CurrencyRate =
     CurrencyRate(
